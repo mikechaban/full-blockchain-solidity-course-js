ad577b7fa8db86a052f9de32a68ccdfd33f084a7
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2023-11-09 20:12:38 -0500

    added AggregatorV3Interface from Chainlink

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
 
     uint256 public minimumUsd = 50;
@@ -12,6 +14,22 @@ contract FundMe {
         require(msg.value >= minimumUsd, "Didn't send enough!");
     }
     
+    function getPrice() public view returns(uint256) {
+        // ABI: 
+        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        return uint256(price * 1e10); // 1*10
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate() public {}
+
     // function withdraw() {}
 
 }
