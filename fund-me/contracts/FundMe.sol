7db3b793b4b28ac09af9559c5b08ac00f12ae231
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2023-11-10 20:26:23 -0500

    added library PriceConverter

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -1,34 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
 
-    uint256 public minimumUsd = 50;
+    using PriceCoverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
 
         // 1. How do we send ETH to this address
-        require(msg.value >= minimumUsd, "Didn't send enough!");
+        // require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
-    
-    function getPrice() public view returns(uint256) {
-        // ABI: 
-        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        // ETH in terms of USD
-        return uint256(price * 1e10); // 1*10
-    }
-
-    function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-
-    function getConversionRate() public {}
 
     // function withdraw() {}
 
---
+++ fund-me/contracts/PriceConverter.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceCoverter {
+    
+    function getPrice() public view returns(uint256) {
+        // ABI: 
+        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        return uint256(price * 1e10); // 1*10
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+}
