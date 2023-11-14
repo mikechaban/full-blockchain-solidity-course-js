ac57443755a63b6be404f6d1c3dd65525bafa38f
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2023-11-13 20:51:38 -0500

    added constructor and modifier

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -12,15 +12,55 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
 
+        
         // 1. How do we send ETH to this address
-        // require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!");
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    // function withdraw() {}
+    function withdraw() public onlyOwner {
+        /* starting index, ending index, step amount */
+        // 0, 10, 1
+
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        // reset the array
+        funders = new address[] (0);
+
+        // actually withdraw the funds
+
+        
+        // transfer
+        // ⬇️
+        // payable(msg.sender).transfer(address(this).balance);
+
+
+        // send
+        // ⬇️
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failled");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "The sender is not the owner!");
+        _;
+    }
 
 }
