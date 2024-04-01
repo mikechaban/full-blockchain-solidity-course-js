e0c11779f20494e33dea98a6f4020e8a5719890d
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-03-31 22:40:41 -0400

    Added "multiple funders" test

--- hardhat-fund-me/test/unit/FundMe.test.js
+++ hardhat-fund-me/test/unit/FundMe.test.js
@@ -94,11 +94,46 @@ describe("FundMe", function () {
                 )
                 await fundMeConnectedContract.fund({ value: sendValue })
             }
+
             const startingFundMeBalance = await fundMe.provider.getBalance(
                 fundMe.address
             )
             const startingDeployerBalance = await fundMe.provider.getBalance(
                 deployer
+            )
+
+            // Act
+            const transactionResponse = await fundMe.withdraw()
+            const transactionReceipt = await transactionResponse.wait(1)
+            const { gasUsed, effectiveGasPrice } = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            // Assert
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // Assert #2
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                // startingFundMeBalance + starterDeployerBalance
+                // For BigNumber:
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+
+            // Make sure that funders are reset properly
+            await expect(fundMe.funders(0)).to.be.reverted
+
+            for (i = 1; i < 6; i++) {
+                assert.equal(
+                    await fundMe.addressToAmountFunded(accounts[i].address),
+                    0
+                )
+            }
         })
     })
 })
