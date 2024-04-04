93bcae48be5489ff4aa307adc8536b7ac4716f9c
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-04-03 23:15:24 -0400

    created Raffle contract

---
+++ hardhat-lottery/contracts/Raffle.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle is VRFConsumerBaseV2 {
+    /* State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // we could do:
+        // require(msg.value > i_entranceFee, "Not enough ETH!")
+
+        // but instead we'll do a custom error cause it's more gas efficient
+
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+
+        s_players.push(payable(msg.sender));
+
+        // Events
+        emit RaffleEntered(msg.sender);
+    }
+
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /* View / Pure Functions */
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
