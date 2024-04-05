6d194e7633657e9f89af112593d6dc2af062bb0b
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-04-04 22:44:57 -0400

    added requestRandomWinner function from Chainlink VRF

--- hardhat-lottery/contracts/Raffle.sol
+++ hardhat-lottery/contracts/Raffle.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.6;
 
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFCoordinatorV2.sol";
 
 error Raffle__NotEnoughETHEntered();
 
@@ -9,12 +10,29 @@ contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_VRFCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callBackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     /* Events */
     event RaffleEntered(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address VRFCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(VRFCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_VRFCoordinator = VRFCoordinatorV2Interface(VRFCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callBackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -33,7 +51,16 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RaffleEntered(msg.sender);
     }
 
-    function requestRandomWinner() external {}
+    function requestRandomWinner() external {
+        uint256 requestId = i_VRFCoordinator.requestRandomWords(
+            i_gasLane, // gasLane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callBackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
 
     function fulfillRandomWords(
         uint256 requestId,
