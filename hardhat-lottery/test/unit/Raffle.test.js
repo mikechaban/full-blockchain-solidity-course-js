965cb0fee14af34906b29bba344375f544a65832
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-04-11 12:53:43 -0400

    undefined error

---
+++ hardhat-lottery/test/unit/Raffle.test.js
@@ -0,0 +1,34 @@
+const { assert, expect } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Tests", async function () {
+          let raffle, raffleContract, vrfCoordinatorV2Mock, raffleEntranceFee, interval, player // , deployer
+
+          beforeEach(async () => {
+              accounts = await ethers.getSigners() // could also do with getNamedAccounts
+              //   deployer = accounts[0]
+              player = accounts[1]
+              await deployments.fixture(["mocks", "raffle"])
+              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+              raffleContract = await ethers.getContract("Raffle")
+              raffle = raffleContract.connect(player)
+              raffleEntranceFee = await raffle.getEntranceFee()
+              interval = await raffle.getInterval()
+          })
+
+          describe("constructor", () => {
+              it("intitializes the raffle correctly", async () => {
+                  // Ideally, we'd separate these out so that only 1 assert per "it" block
+                  // And ideally, we'd make this check everything
+                  const raffleState = (await raffle.getRaffleState()).toString()
+                  assert.equal(raffleState, "0")
+                  assert.equal(
+                      interval.toString(),
+                      networkConfig[network.config.chainId]["keepersUpdateInterval"]
+                  )
+              })
+          })
+      })
