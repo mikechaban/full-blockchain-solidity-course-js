c3126435aa8fa1c61affb5a5c9ca8d1da152d9ea
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-04-18 10:11:49 -0400

    added LotteryEntrance and a script to get addresses and abi, issue commit

---
+++ nextjs-smartcontract-lottery/components/LotteryEntrance.jsx
@@ -0,0 +1,39 @@
+// have a function to enter the lottery
+import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
+
+export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainIdHex in contractAddresses ? contractAddresses[chainId][0] : null
+
+    // const { runContractFunction: enterRaffle } = useWeb3Contract({
+    //     abi: abi,
+    //     contractAddress: raffleAddress, // specify the networkId
+    //     functionName: "enterRaffle",
+    //     params: {},
+    //     msgValue: null,
+    // })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            async function updateUi() {
+                const entranceFeeFromContract = await getEntranceFee()
+                console.log(entranceFeeFromContract)
+            }
+            updateUi()
+        }
+    }, [isWeb3Enabled])
+
+    return <div>Hi from Lottery Entrance</div>
+}
--- nextjs-smartcontract-lottery/pages/index.js
+++ nextjs-smartcontract-lottery/pages/index.js
@@ -8,6 +8,7 @@ import Image from "next/image"
 import styles from "../styles/Home.module.css"
 // import ManualHeader from "../components/ManualHeader"
 import Header from "../components/Header"
+import LotteryEntrance from "../components/LotteryEntrance"
 
 export default function Home() {
     return (
