323d1883763b8d4b669bccfe2a0ea26bc235ac1c
Mike Chabanovskyi <mikechabanovskyi@gmail.com>
2024-05-14 18:32:06 -0400

    commented out Image

--- nextjs-smartcontract-lottery/pages/index.js
+++ nextjs-smartcontract-lottery/pages/index.js
@@ -4,8 +4,8 @@
 // backend Js is a little different from frontend Js
 
 import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
+// import Image from "next/image"
+// import styles from "../styles/Home.module.css"
 // import ManualHeader from "../components/ManualHeader"
 import Header from "../components/Header"
 import LotteryEntrance from "../components/LotteryEntrance"
@@ -19,8 +19,8 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.png" />
             </Head>
-            <Header/>
-            <LotteryEntrance/>
+            <Header />
+            <LotteryEntrance />
         </>
     )
 }
